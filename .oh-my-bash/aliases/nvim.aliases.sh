977b8dcc6a638fd6bf59fe6e75ec59bc188fe685
Narmis-E <narmisecurb@gmail.com>
2024-05-18 10:05:16 +0930

    remove nvim aliases

--- .oh-my-bash/aliases/nvim.aliases.sh
+++
@@ -1,9 +0,0 @@
-#! bash oh-my-bash.module
-#  ---------------------------------------------------------------------------
-
-# nvim
-alias nvim='nvim'
-alias v='nvim'
-alias vs="nvim $HOME/sway-minimal/sway/config"
-alias vf='vifm'
-alias sv='sudoedit'
