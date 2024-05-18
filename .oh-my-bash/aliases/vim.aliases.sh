be6ef3199bb3efc4c87a85ba0b27a07458957f3b
Narmis-E <narmisecurb@gmail.com>
2024-05-18 10:05:25 +0930

    added vim aliases

---
+++ .oh-my-bash/aliases/vim.aliases.sh
@@ -0,0 +1,8 @@
+#! bash oh-my-bash.module
+#  ---------------------------------------------------------------------------
+
+# nvim
+alias nvim='vim'
+alias v='vim'
+alias vs="vim $HOME/sway-minimal/sway/config"
+alias sv='sudoedit'
